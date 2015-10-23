4d61c78e770b49e1bf9cf46e2ed91d5b74d56334
Alexander James Phillips <ajp.global@gmail.com>
2015-10-23 14:20:23 +0100

    Algorithm iterate method accepts arguments

--- src/algo/algorithm.ts
+++ src/algo/algorithm.ts
@@ -8,14 +8,14 @@ interface IterationResult {
 class Algorithm {
   protected iterationResult: IterationResult;
 
-  public iterate(): IterationResult {
+  public iterate(args: any = {}): IterationResult {
     if(!this.iterationResult || !this.iterationResult.finished) {
-      this.iterationResult = this._iterate();
+      this.iterationResult = this._iterate(args);
     }
     return this.iterationResult;
   }
 
-  protected _iterate(): IterationResult {
+  protected _iterate(args: any): IterationResult {
     throw new Error('Subclasses must implement "_iterate"');
   }
 
